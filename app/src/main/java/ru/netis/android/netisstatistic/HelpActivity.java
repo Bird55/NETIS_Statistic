2c95ebe925d597cc5a51bfa38d702f35e47d15bf
Alexey Solovyev <bird55@narod.ru>
2015-06-22 18:32:50 +0300

    Samething

--- app/src/main/java/ru/netis/android/netisstatistic/HelpActivity.java
+++ app/src/main/java/ru/netis/android/netisstatistic/HelpActivity.java
@@ -1,12 +1,13 @@
 package ru.netis.android.netisstatistic;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HelpActivity extends ActionBarActivity {
+public class HelpActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
